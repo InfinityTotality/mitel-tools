b44be323ce06ba50624653b6ea6e3fb569b8c317
Joshua Timmer <mrjoshuatimmer@gmail.com>
2015-11-06 17:10:38 -0500

    Added extension concurrence script

---
+++ extensionconcurrence.py
@@ -0,0 +1,99 @@
+import sys
+import datetime
+import collections
+import smdrreader
+from os import path
+
+
+def duration_to_seconds(duration):
+    duration_parts = duration.split(':')
+    hours = int(duration_parts[0])
+    minutes = int(duration_parts[1])
+    seconds = int(duration_parts[2])
+    seconds += minutes * 60
+    seconds += hours * 60 * 60
+    return seconds
+
+
+def parse_lines(smdr_lines, extension_data):
+    for line in smdr_lines:
+        try:
+            event = smdrreader.SMDREvent(line.decode('UTF-8'))
+        except smdrreader.InvalidInputException as e:
+            print(str(e))
+            print(line)
+            continue
+        extension = event.called_party
+        if extension in extension_data:
+            event_start_time = datetime.datetime.strptime(event.time, '%H:%M:%S')
+            event_duration = duration_to_seconds(event.duration)
+            i = 0
+            while i < event_duration:
+                event_current_time = event_start_time + datetime.timedelta(seconds=i)
+                event_timedate_string = '{} {}'.format(event.date,
+                        event_current_time.strftime('%H:%M:%S'))
+                if extension_data[extension][event_timedate_string] == 1:
+                    print('Duplicate call events detected for extension' +
+                          '{} on {} at {}'.format(extension,event.date,event.time))
+                extension_data[extension][event_timedate_string] = 1
+                i += 1
+
+
+def combine_extensions(extension_data):
+    output_lines = []
+    combined_data = collections.defaultdict(int)
+    for extension in extension_data:
+        for datetime in extension_data[extension]:
+            combined_data[datetime] += 1
+    return combined_data
+
+
+def print_results(data, number_of_extensions):
+    sorted_data = sorted(data.items(), key=lambda item: item[0])
+    all_in_use_events = []
+    high_use_events = []
+
+    for time,count in sorted_data:
+        if count == number_of_extensions:
+            all_in_use_events.append((time,count))
+        elif count > number_of_extensions * 3 / 4:
+            high_use_events.append((time,count))
+        print('{}  {}'.format(time, count))
+    
+    print('{} high usage events:'.format(len(high_use_events)))
+    for time,count in high_use_events:
+        print('{}  {}'.format(time, count))
+    print('{} all in use events:'.format(len(all_in_use_events)))
+    for time,count in all_in_use_events:
+        print('{}  {}'.format(time, count))
+
+
+def summarize(smdr_reader, extensions):
+    extension_data = dict((extension, collections.defaultdict(int)) for extension in extensions)
+
+    for file in smdr_reader.file_reader():
+        parse_lines(file, extension_data)
+    combined_data = combine_extensions(extension_data)
+    print_results(combined_data, len(extensions))
+
+
+
+# script body
+
+start_date = sys.argv[1]
+end_date = sys.argv[2]
+data_directory = sys.argv[3]
+
+try:
+    smdr_reader = smdrreader.SMDRReader(data_directory, start_date, end_date)
+except smdrreader.InvalidInputException as e:
+    print('Error: ' + str(e))
+    exit(1)
+
+extensions = sys.argv[4:]
+
+for extension in extensions:
+    if not extension.isnumeric():
+        raise smdrreader.InvalidInputException('Invalid extension: "{}"'.format(extension))
+
+summarize(smdr_reader, extensions)
